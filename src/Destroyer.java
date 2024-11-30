994e9a3830f430e14a8229b574708aa8e35c823c
3thanl1n <120241714+3thanl1n@users.noreply.github.com>
2024-11-30 13:30:00 -0500

    Created Destroyer derived class from abstract ship class and overriding getshiptype methods.

---
+++ src/Destroyer.java
@@ -0,0 +1,11 @@
+public class Destroyer extends Ship {
+    public Destroyer(){
+        length= 2;
+        hit= new boolean[2];
+    }
+
+    @Override
+    public String getShipType() {
+        return "Destroyer";
+    }
+}
